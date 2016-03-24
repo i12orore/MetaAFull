ee141d7a20720a5c304349fc6eb5a7361758431a
i12orore <i12orore@uco.es>
2016-03-24 16:09:47 +0100

    Añado el inicio de x1, que es el First Improvement

---
+++ x1NeighExplorator.hpp
@@ -0,0 +1,24 @@
+// X1NeighExplorator
+#ifndef __X1_NEIGH_EXPLORATOR__
+#define __X1_NEIGH_EXPLORATOR__
+//Suponiendo X1 Fast Improvement Strategy --> Mira a los vecinos y el primero que se cambia es el que sea mejor al que estoy buscando
+//el X2 será el Best Improvement Strategy --> Mira todos los vecinos y coge el mejor
+class FastImprovementStrategyNeighExplorator
+{
+private:
+
+
+
+public:
+
+
+
+
+
+
+
+};
+
+
+
+#endif
